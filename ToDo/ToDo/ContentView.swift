bd206a4bbed044bf1f8b850ebfec4704c797f614
city1616 <city1616@naver.com>
2020-07-20 19:33:25 +0900

    first

---
+++ ToDo/ToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by SeungWoo Mun on 2020/07/20.
+//  Copyright © 2020 SeungWoo Mun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
