529a21aefa6e2473b912479a610c888049f2273a
city1616 <city1616@naver.com>
2020-07-29 20:53:40 +0900

    ios14 Test

---
+++ Test/Test/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by SeungWoo Mun on 2020/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: {}) {
+                Text("Hello")
+            }
+            Text("Test")
+            Menu("Menu") {
+                Text("first")
+                Text("second")
+                Menu("aaaa") {
+                    Text("a")
+                    Text("aa")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
