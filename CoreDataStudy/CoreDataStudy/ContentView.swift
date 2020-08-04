a94ae953702214c8a990258dcc2d31bf7bd263a3
city1616 <city1616@naver.com>
2020-08-04 16:35:07 +0900

    Core Data Study

--- CoreDataStudy/CoreDataStudy/ContentView.swift
+++ CoreDataStudy/CoreDataStudy/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
     var body: some View {
@@ -16,5 +17,51 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    
+    // stateobject is parent of the object
+    @StateObject var model = dataModel()
+    
+    var body: some View {
+        VStack {
+            List(model.data, id: \.objectID) { obj in
+                
+                // extracting data from object
+                Text("a")
+            }
+        }
+    }
+}
+
+// MVVM Pattern
+class dataModel: ObservableObject {
+    @Published var data: [NSManagedObject] = []
+    @Published var txt = ""
+    
+    init() {
+        
+    }
+    
+    func readData() {
+        
+    }
+    
+    func writeData() {
+        
+    }
+    
+    func deleteData() {
+        
+    }
+    
+    func updateData() {
+        
+    }
+    
+    func getValue(obj: NSManagedObject) -> String {
+        return obj.value(forKey: "value") as! String
     }
 }
