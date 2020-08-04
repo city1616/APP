8380e09548302116b9c44ee967246b972e548122
city1616 <city1616@naver.com>
2020-08-04 16:17:53 +0900

    Create Core Data Study

---
+++ CoreDataStudy/CoreDataStudy/CoreDataStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataStudyApp.swift
+//  CoreDataStudy
+//
+//  Created by SeungWoo Mun on 2020/08/04.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
