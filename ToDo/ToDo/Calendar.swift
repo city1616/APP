c2b09b9162bc536337b3f698e4a78257e4410b68
city1616 <city1616@naver.com>
2020-08-08 23:51:44 +0900

    ToDo App Calendar

--- ToDo/ToDo/Calendar.swift
+++ ToDo/ToDo/Calendar.swift
@@ -18,20 +18,22 @@ struct Calendar: View {
 //        }
         VStack {
             Form {
-                Section(header: Text("Due Date")) {
+                Section(header: Text("Due Date").fontWeight(.bold)) {
                     DatePicker("When is your birthday?", selection: $selectDate, displayedComponents: .date)
                         // .labelsHidden()
                         .foregroundColor(.green)
                         // .background(with: .black)
                 }
+                .foregroundColor(.pink)
                 
-                DatePicker("", selection: $selectDate, displayedComponents: .date)
-                Text("Your selected date: \(selectDate)")
-                    .foregroundColor(.red)
-                    .background(Color.green)
-                
-                Spacer(minLength: 0)
-                
+                Section(header: Text("Date")) {
+                    DatePicker("", selection: $selectDate, displayedComponents: .date)
+                    Text("Your selected date: \(selectDate, formatter: self.DateFormat())")
+                        .foregroundColor(.pink)
+                        .fontWeight(.bold)
+                        // .background(Color.green)
+                }
+          
             }
             .frame(height: 350)
             .background(Color.blue)
@@ -40,6 +42,15 @@ struct Calendar: View {
             Spacer()
         }
     }
+    func DateFormat() -> DateFormatter {
+        let formatter = DateFormatter()
+        // formatter.dateFormat = "dd-MM-YYYY"
+        // formatter.dateFormat = "HH : mm"
+        // formatter.timeStyle = .long
+        formatter.dateStyle = .long
+        return formatter
+    }
+    
 }
 
 struct Calendar_Previews: PreviewProvider {
