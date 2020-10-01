86c526bf1ac51c04aecd6a21c7839552602ec4c1
city1616 <city1616@naver.com>
2020-10-01 12:52:10 +0900

    ADD AR_TEST

---
+++ AR_TEST/AR_TEST/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  AR_TEST
+//
+//  Created by SeungWoo Mun on 2020/10/01.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
